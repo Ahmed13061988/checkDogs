57352fe7e82fd07be0d1c21d9e6b0d84d39c599b
Ahmed Hussein <wolktheline@yahoo.com>
2022-09-20 10:46:25 -0600

    done

--- dogCheck.js
+++ dogCheck.js
@@ -3,4 +3,19 @@
 const dogsJulia = [3, 5, 2, 12, 7];
 const dogsKate = [4, 1, 15, 8, 3];
 
-const checkDogs = function (dogsJulia, dogsKate) {};
+const checkDogs = function (dogsJulia, dogsKate) {
+  const catsJulia = dogsJulia.slice();
+  const onlyDogsJulia = catsJulia.splice(1, 2);
+
+  const both = onlyDogsJulia.concat(dogsKate);
+  console.log(both);
+  both.forEach(function (dog, i) {
+    const adult =
+      dog >= 3
+        ? `Dog number ${i + 1} is an adult`
+        : `Dog number ${i + 1} is still a puppy 🐶`;
+    console.log(adult);
+  });
+};
+
+checkDogs(dogsJulia, dogsKate);
